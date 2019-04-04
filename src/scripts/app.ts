ce631d672105ea71b3b69e5aecc79ae632ea85dc
Meet Parikh <m.parikh@reply.com>
2019-04-04 17:37:19 +0100

    Implementing Mondrian class

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,7 +1,25 @@
 import * as $ from "jquery";
 
+class Mondrian {
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+
+  constructor() {
+    let canvas = $("#art").get(0) as HTMLCanvasElement;
+    let context = canvas.getContext("2d");
+
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+
+    this.canvas = canvas;
+    this.context = context;
+  }
+}
+
 function main() {
-  $("#art").html("Mother Nature !!!!");
+  let mondrian = new Mondrian();
   console.log("Mother Nature !!!!");
 }
 
