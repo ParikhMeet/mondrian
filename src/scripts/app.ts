94217cf8d8d42d51d0c84e7b48dc1a5060017cd4
Meet Parikh <meet.parikh@gmail.com>
2019-04-08 22:55:14 +0100

    Refactoring drawing functions

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -8,6 +8,8 @@ class Mondrian {
   constructor() {
     let canvas = $("#art").get(0) as HTMLCanvasElement;
     let context = canvas.getContext("2d");
+    console.log(`Width: ${canvas.width}`);
+    console.log(`Height: ${canvas.height}`);
 
     context.strokeStyle = "black";
     context.lineWidth = 1;
@@ -15,20 +17,21 @@ class Mondrian {
     this.canvas = canvas;
     this.context = context;
 
-    this.drawRect();
+    this.drawRect(0, 0, 300, 150);
+    this.drawRandomRect()
   }
 
   private generateRandomNumber(minVal: number, maxVal: number): number {
     return Math.floor(Math.random() * maxVal) + minVal
   }
 
-  private drawRect() {
-    console.log(`Width: ${this.canvas.width}`);
-    console.log(`Height: ${this.canvas.height}`);
-
-    this.context.fillRect(0, 0, 300, 150);
+  private drawRandomRect() {
     this.context.fillStyle = `rgb(${this.generateRandomNumber(0, 255)}, ${this.generateRandomNumber(0, 255)}, ${this.generateRandomNumber(0, 255)})`
-    this.context.fillRect(0, 0, this.generateRandomNumber(0, 300), this.generateRandomNumber(0, 150));
+    this.drawRect(0, 0, this.generateRandomNumber(0, 300), this.generateRandomNumber(0, 150))
+  }
+
+  private drawRect(x: number, y: number, height: number, width: number) {
+    this.context.fillRect(x, y, height, width);
   }
 }
 
