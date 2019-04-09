16ad0bd31318d8c4772b1b4dc8a29a481b7f27c1
Meet Parikh <m.parikh@reply.com>
2019-04-09 16:49:19 +0100

    sketch it

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -12,8 +12,8 @@ type Rectangle = {
 class Mondrian {
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
-  private strokeStyle = "black";
   private lineWidth = 1;
+  private minimumSpacing = 5;
 
   constructor() {
     let canvas = $("#art").get(0) as HTMLCanvasElement;
@@ -21,22 +21,84 @@ class Mondrian {
     console.log(`Width: ${canvas.width}`);
     console.log(`Height: ${canvas.height}`);
 
-    context.strokeStyle = this.strokeStyle;
     context.lineWidth = this.lineWidth;
 
     this.canvas = canvas;
     this.context = context;
-
-    this.drawBaseRect();
-    this.drawRect({
-      x: 0,
-      y: 0,
-      width: _.random(300),
-      height: _.random(150)
-    });
   }
 
-  private drawBaseRect() {
+  generate() {
+    const baseRect = this.drawBaseRect();
+    this.processRect(baseRect);
+  }
+
+  private processRect(rect: Rectangle) {
+    const breakByWidth = _.sample([true, false]);
+    if (breakByWidth && rect.width >= 2 * this.minimumSpacing) {
+      //break by width
+      const rects: [Rectangle, Rectangle] = this.breakRect(rect, true);
+      rects.forEach(it => this.processRect(it));
+    } else if (rect.height >= 2 * this.minimumSpacing) {
+      //break by height
+      const rects: [Rectangle, Rectangle] = this.breakRect(rect, false);
+      rects.forEach(it => this.processRect(it));
+    } else {
+      //Rect Already processed
+    }
+  }
+
+  private breakRect(
+    rect: Rectangle,
+    breakByWidth: boolean
+  ): [Rectangle, Rectangle] {
+    const breakRatio = breakByWidth
+      ? this.prepareBreakRatio(rect.width)
+      : this.prepareBreakRatio(rect.height);
+
+    if (breakByWidth) {
+      const rect1 = {
+        x: rect.x,
+        y: rect.y,
+        width: Math.floor(rect.width * breakRatio),
+        height: rect.height
+      };
+      const rect2 = {
+        x: rect.x + Math.floor(rect.width * breakRatio),
+        y: rect.y,
+        width: rect.width - Math.floor(rect.width * breakRatio),
+        height: rect.height
+      };
+      this.drawRect(rect1);
+      this.drawRect(rect2);
+      return [rect1, rect2];
+    } else {
+      const rect1 = {
+        x: rect.x,
+        y: rect.y,
+        width: rect.width,
+        height: Math.floor(rect.height * breakRatio)
+      };
+      const rect2 = {
+        x: rect.x,
+        y: rect.y + Math.floor(rect.height * breakRatio),
+        width: rect.width,
+        height: rect.height - Math.floor(rect.height * breakRatio)
+      };
+      this.drawRect(rect1);
+      this.drawRect(rect2);
+      return [rect1, rect2];
+    }
+  }
+
+  private prepareBreakRatio(distance: number): number {
+    if (distance == 2 * this.minimumSpacing) {
+      return 0.5;
+    } else {
+      return _.random(0, 1, true);
+    }
+  }
+
+  private drawBaseRect(): Rectangle {
     //Width: 300, Height: 150
     const baseRect: Rectangle = {
       x: 0,
@@ -45,6 +107,7 @@ class Mondrian {
       height: this.canvas.height
     };
     this.drawRect(baseRect);
+    return baseRect;
   }
 
   private generateRandomRGBStyle(): string {
@@ -62,6 +125,7 @@ class Mondrian {
 
 function main() {
   let mondrian = new Mondrian();
+  mondrian.generate();
   console.log("Mother Nature !!!!");
 }
 
